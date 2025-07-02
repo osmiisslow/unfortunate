c5e50afd9a1f1ef0aa4bfe147606557bede3bafc
osmi <osmi@goober.cloud>
2025-07-02 14:05:16 -0500

    make user able to add quotes

--- src/add_quote.rs
+++ src/add_quote.rs
@@ -1,5 +1,5 @@
 use std::io::Read;
-use curl::easy::Easy;
+use curl::easy::{Easy, List};
 use whoami::fallible::*;
 pub fn add_quote(quote: &String) {
     let user = username().unwrap();
@@ -7,8 +7,14 @@ pub fn add_quote(quote: &String) {
     let format = format!("{user}@{host}: \"{quote}\"");
     let mut data = format.as_bytes();
     
+    // set content type to plain text
+    let mut list = List::new();
+    list.append("Content-Type: text/plain").unwrap();
+
+
     let mut easy = Easy::new();
     easy.url("localhost:8080").unwrap(); // TODO: For the love of god change this after we're finished with server work
+    easy.http_headers(list).unwrap();
     easy.post(true).unwrap();
     easy.post_field_size(data.len() as u64).unwrap();
 
@@ -16,5 +22,5 @@ pub fn add_quote(quote: &String) {
     transfer.read_function(|buf| {
         Ok(data.read(buf).unwrap_or(0))
     }).unwrap();
-    transfer.perform().unwrap();
+    transfer.perform().expect("can't add quote! Maybe server is down?");
 }
